710c88359ff51918c497dcbbc40ec13d55f5209f
Yegor Kirsanov <ekirsanov@wowworks.ru>
2021-12-20 22:54:01 +0400

    init

---
+++ next.config.mjs
@@ -0,0 +1,20 @@
+// ----------------------------------------------------------------------
+
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  // i18n: {
+  //   defaultLocale: 'en',
+  //   locales: ['en', 'de', 'fr'],
+  // },
+  env: {
+    DEV_API: 'http://localhost:8888',
+    PRODUCTION_API: 'https://zone-test-api.vercel.app',
+    GOOGLE_API: '',
+  },
+  images: {
+    domains: ['flagcdn.com'],
+  },
+};
+
+export default nextConfig;
